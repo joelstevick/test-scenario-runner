08d814a13d18333c5f67fbfb08528a1219c9315b
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-03-31 16:35:31 -0400

    context data should be passed to children

--- src/run-scenario.ts
+++ src/run-scenario.ts
@@ -4,15 +4,20 @@ import { Scenario } from './model/scenario.interface';
 
 export async function runScenario(scenario: Scenario, contextData: any) {
 
-    if (typeof scenario.runner === 'function') {
-        const contextFn = getContext(scenario);
+    // get the context
+    const contextFn = getContext(scenario);
 
-        if (typeof contextFn === 'function') {
-            contextData = contextFn();
-        }
+    if (typeof contextFn === 'function') {
+        contextData = contextFn();
+    }
+    
+    // optional runner
+    if (typeof scenario.runner === 'function') {
 
         await scenario.runner(contextData);
     }
+
+    // recurse 
     if (scenario.children && scenario.children.length > 0) {
         for (const childScenario of scenario.children) {
             await runScenario(childScenario, contextData);
