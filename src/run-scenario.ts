5f2e42b51245a530d22c3599656774affca2d5e0
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-03-31 15:58:19 -0400

    increase coverage for runner tests

--- src/run-scenario.ts
+++ src/run-scenario.ts
@@ -3,7 +3,7 @@ import { Context } from './model/context.interface';
 import { Scenario } from './model/scenario.interface';
 
 export async function runScenario(scenario: Scenario, context: Context | null | undefined) {
-  if (typeof scenario?.runner === 'function') {
+  if (typeof scenario.runner === 'function') {
     const contextFn = getContext(scenario);
 
     if (typeof contextFn === 'function') {
