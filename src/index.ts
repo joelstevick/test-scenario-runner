a1d0f3f8f499de0981a10bf3a9069ddf99980204
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-04-01 11:23:36 -0400

    fix problem with api and module resolution

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { ContextFactory, Context, Runner, Scenario } from './model';
-import { runScenario } from './run-scenario';
+import { runScenario as _runScenario } from './run-scenario';
 import { getContext } from './get-context';
 
 export { ContextFactory, Context, Runner, Scenario };
-export { runScenario };
 export { getContext };
+
+export function runScenario(scenario: Scenario, context: Context) {
+    return _runScenario(scenario, context);
+}
