1386d6b2ab87136a78c31c0ec3311bb0f0a75e73
Joel Stevick <jstevickic-1.com@Joels-IC-1-MacBook-Pro.local>
2021-04-01 11:02:04 -0400

    1.0.15

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { ContextFactory, Context, Runner, Scenario } from './model';
 import { runScenario } from './run-scenario';
 import { getContext } from './get-context';
 
-
 export { ContextFactory, Context, Runner, Scenario };
 export { runScenario };
 export { getContext };
